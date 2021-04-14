a79caa68680bf01e8a4d5922d614eb88bf5d8900
Arvind Kant <kanta1311@gmail.com>
2021-04-14 11:53:37 +0530

    fcm added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  // {
+  //   path: 'details',
+  //   loadChildren: () => import('./pages/details/details.module').then( m => m.DetailsPageModule)
+  // },
+  {
+    path: 'home/:id',
+    loadChildren: () => import('./pages/details/details.module').then(m => m.DetailsPageModule)
   },
 ];
 
